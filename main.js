11878ecbd690111b0dcbcaae3f66d7731a6e4803
Marco Antonio González <mgonzalezdiez@gmail.com>
2020-08-10 19:47:38 -0400

    Refactorizar variantes

--- main.js
+++ main.js
@@ -1,47 +1,57 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-        description: "lorem Ipsum",
-        product: "Longaniza de Brocolli",
+  el: '#app',
+  data: {
+    description: "ProductoGrarantizado ECamp",
+    product: "Longaniza de Brocolli",
+    selectedVariant: {},
+    link: 'longanizas.html',
+    cart: 0,
+    details: ["500 gr", "organica", "libre de colesterol"],
+    variants: [{
+        id: 1,
+        type: "Rojas",
         img: "./assets/index.jpg",
-        lnk: "./longaniza.html",
-        stock: 12,
-        details: ["500 gr", "organica", "libre de colesterol"],
-        variants: [{
-                id: 1,
-                type: "Rojas",
-                img: "./assets/index.jpg"
-            },
-            {
-                id: 2,
-                type: "Blancas",
-                img: "./assets/blanca.jpg"
-            }
-        ],
-        cart: 0
+        stock: 10,
+        default: true
+      },
+      {
+        id: 2,
+        type: "Blancas",
+        img: "./assets/blanca.jpg",
+        stock: 0
+      }
+    ],
+    cart: 0
+  },
+  methods: {
+    updateProduct(variant) {
+      this.selectedVariant = variant;
     },
-    methods: {
-        updateImg(img) {
-            this.img = img;
-        },
-        addToCart() {
-            if (this.stock != 0) {
-                this.cart += 1;
-                this.stock -= 1;
-            }
-        },
-        removeFromCart() {
-            debugger
-            if (this.cart > 0) {
-                this.cart -= 1;
-                this.stock += 1;
-            }
-        }
+    addToCart() {
+      if (this.stock != 0) {
+        this.cart += 1;
+        this.selectedVariant.stock -= 1;
+      }
     },
-    computed: {
-        inStock() {
-            return this.stock > 0 ? true : false;
-
-        }
+    removeFromCart() {
+      if (this.cart > 0) {
+        this.cart -= 1;
+        this.selectedVariant.stock += 1;
+      }
     }
+  },
+  computed: {
+    inStock() {
+      return this.stock > 0 ? true : false;
+    },
+    img() {
+      return this.selectedVariant.img;
+    },
+    stock() {
+      return this.selectedVariant.stock;
+    }
+  },
+  created() {
+    this.selectedVariant = this.variants.find(item => item.default === true)
+  }
 })
