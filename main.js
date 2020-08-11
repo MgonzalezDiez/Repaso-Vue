4dfd7cd927e17cffb70b7c3c5f1e389b47a80dd9
Marco Antonio González <mgonzalezdiez@gmail.com>
2020-08-11 19:57:35 -0400

    Se crea x-template

--- main.js
+++ main.js
@@ -51,37 +51,7 @@ Vue.component("product", {
   created() {
     this.selectedVariant = this.variants.find(item => item.default === true)
   },
-  template: `<div class="row">
-          <div class="col">
-            <img v-bind:src="img" />
-            <!--atributos-->
-          </div>
-          <div class="col pt-3">
-            <h1>{{ product }}</h1>
-            <!--contenido etiqueta-->
-            <h4>{{ description }}</h4>
-            <h4>{{ stock }}</h4>
-            <hr>
-            <p v-if='stock > 10'>En Stock</p>
-            <p v-else-if='stock <= 10 && stock > 0'>Quedan pocas unidades</p>
-            <p v-else>Agotado</p>
-            <ul>
-              <li v-for='(detail, index) in details' :key="index">
-                {{ detail }}
-              </li>
-            </ul>
-            <hr>
-            <h4>Variedades</h4>
-            <div class="pl-3" v-for='variant in variants' :key="variant.id">
-              <p v-on:mouseover="updateProduct(variant)">{{ variant.type }}</p>
-            </div>
-
-            <a class="a__productos" :href="link">Ver detalle</a>
-
-            <button class="btn btn-info btn-sm" :disabled="!inStock" @click="addToCart">Agregar al Carro</button>
-            <button class="btn btn-danger btn-sm" @click="removeFromCart">Sacar del Carro</button>
-          </div>
-        </div>`
+  template: '#product-template'
 })
 var app = new Vue({
   el: '#app',
